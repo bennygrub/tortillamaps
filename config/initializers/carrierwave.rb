238837aacf8e8e1172b4b8bad85230fb045d5d39
bennygrub <blgruber@gmail.com>
2017-02-27 12:28:05 -0800

    added hardcode - need to remove eventually

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
 CarrierWave.configure do |config|
-  if Rails.env != "development"
     config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     ENV['S3_KEY'],                        # required
-      aws_secret_access_key: ENV['S3_SECRET'],                        # required
+      aws_access_key_id:     "AKIAJ3WUKF3SSH2ZLENQ", #ENV['S3_KEY'],                        # required
+      aws_secret_access_key: "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",
+      region:                'us-west-2'#ENV['S3_SECRET'],                        # required
     }
-    config.fog_directory  = ENV['S3_BUCKET_NAME']                          # required
-  end
+    config.fog_directory  = "tortillamaps"#ENV['S3_BUCKET_NAME']                          # required
 end
