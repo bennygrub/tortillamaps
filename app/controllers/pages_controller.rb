412f29be7aad065c49afa9c90c299cbc9a0decd5
bennygrub <blgruber@gmail.com>
2017-04-25 17:22:36 -0700

    going to broad view if no restaurants can be found

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,18 @@ class PagesController < ApplicationController
       marker.title restaurant.name
       marker.json({ :id => restaurant.id, :name => restaurant.name})
     end
-    @location = request.location
+    if request.location.ip == '::1'
+      @location = request.safe_location
+      rest_count = Restaurant.near([34.0900310, -118.3688430], 0.1, :units => :km).map{|a| a}.count
+    else
+      @location = request.safe_location
+      rest_count = Restaurant.near([@location.latitude, @location.longitude], 1, :units => :km).map{|a| a}.count
+    end
+    if rest_count > 0
+      @near_me = "yes"
+    else
+      @near_me = ""
+    end
   end
 
   def about
